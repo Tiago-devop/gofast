2b232fd21c4b6306de74b068755f648809f8e494
Eduardo Deuner <dudufdeuner@gmail.com>
2022-10-03 11:55:04 -0300

    Home texto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -34,72 +34,11 @@ const Home = () => (
       </Carousel>
       </div>
       <main>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          minus a consequuntur atque, iusto, neque sit corrupti, omnis possimus
-          porro esse modi culpa qui eum voluptates delectus vel recusandae.
-          Quos.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-          saepe, culpa ratione quidem ducimus optio sequi laudantium atque nihil
-          ad dolorem perferendis distinctio odio nulla, at quam iusto ullam
-          voluptatibus!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          voluptas inventore nobis laboriosam cum nesciunt minus dicta harum.
-          Libero incidunt tenetur sed cupiditate! Error natus soluta quaerat
-          architecto odio quisquam!
-        </p>
+
+       <h2>GoFast Auto</h2>
+
+       <p>Somos uma empresa tradicional no ramo de veículos, trabalhamos para manter sempre um atendimento bom e de qualidade nos nossos clientes. Buscando oferecer variedades de produtos.</p>
+       
       </main>
     </Wrapper>
   </>
