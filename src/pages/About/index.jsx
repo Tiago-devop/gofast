a18c03bbbc4c1a277958bd5d8408c1999fe0c7ff
Eduardo Deuner <dudufdeuner@gmail.com>
2022-10-03 11:39:03 -0300

    About texto

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,8 +3,29 @@ import { Title } from "./About.style";
 const About = () => (
   <>
   <div>
+
   <Title>Quem somos nós ?</Title>
+
+  <h3>Empresa GoFast Auto</h3>
+
+  <p>Fundada em 2022, a empresa GoFast Auto é uma empresa especializada em veículos novos e seminovos. Localizada em São Paulo, nossa concessionária conta com um espaço agradável para melhor atender nossos clientes, mantendo a lealdade e a tradição que a empresa traz em suas vendas, tornando- se referência no mercado nacional e importado.</p>
+
+  <h4>Equipe</h4>
   
+  <p>Nossa equipe é especializada e atualizada, buscando fidelizar nossos clientes com atendimento sério, de qualidade e honestidade. Procuramos sempre agregar serviços para melhorar a satisfação dos nossos clientes.</p>
+
+  <h4>Missão</h4>
+
+  <p>A nossa missão é prestar serviços necessários na hora da compra e financiamento, buscando a satisfação em atender as necessidades e superar as expectativas dos nossos clientes.</p>
+
+  <h4>Visão</h4>
+
+  <p>Ser uma empresa referência e reconhecida como a melhor opção pelos clientes, pelo parceiro do ramo na hora da venda de veículos nacional e importados.
+  </p>
+
+  <h4>Valores</h4>
+
+  <p>Comprometimentos, Integridade, Qualidade em serviços, Praticidade, Ética, Resultados e Santificação para nossos clientes.</p>
   </div>
   </>
 )
